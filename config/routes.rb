8afe63f03d4e668400034ab4f4ea31d2d63e3225
wonderer007 <alihaider907@gmail.com>
2015-11-30 22:46:06 +0500

    layout added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   resource :welcome, :only => [:index]
 
+  get '/demo' => "welcome#demo"
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
 
